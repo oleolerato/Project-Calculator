9b59e2feb81085d4ab293f5f13476bc55137e269
oleolerato <olerato.mosikidi@gmail.com>
2022-09-11 16:12:36 +0200

    Create functions for the basic operations

--- script.js
+++ script.js
@@ -11,6 +11,34 @@ let operands = [firstOperand, secondOperand];
 let answer = 0;
 let shouldResetScreen = true;
 
+function add(a,b){
+  answer = a + b
+  console.log(answer)
+}
+
+add(2,2)
+
+function subtract(a,b){
+  answer = a - b
+  console.log(answer)
+}
+
+subtract(10, 5)
+
+function multiply(a,b){
+  answer = a * b
+  console.log(answer)
+}
+
+multiply(6, 2)
+
+function divide(a,b){
+  answer = a / b
+  console.log(answer)
+}
+
+divide(24,2)
+
 numerals.forEach((button) => {
   let value = button.value;
   button.textContent = button.value;

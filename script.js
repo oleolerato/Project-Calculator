6d1ea3cbdcd65e1514929d5095d4c325bac1e492
oleolerato <olerato.mosikidi@gmail.com>
2022-09-11 16:02:51 +0200

    Add forEach Iterations for button NodeLists

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const numerals = document.querySelectorAll('.numerals button');
 const operators = document.querySelectorAll('.operators button');
 const clrAndDel = document.querySelectorAll('.clearAndDel button');
 
-let currentOperation = none;
-let firstOperand = 0
-let secondOperand = 0
+let currentOperation = 'none';
+let firstOperand = 0;
+let secondOperand = 0;
+let operands = [firstOperand, secondOperand];
+let answer = 0;
+let shouldResetScreen = true;
+
+numerals.forEach((button) => {
+  let value = button.value;
+  button.textContent = button.value;
+  button.onclick = () => mainDisplay.textContent += value;
+});
+operators.forEach((button) => {
+  let value = button.value;
+  button.textContent = button.value;
+  button.onclick = () => mainDisplay.textContent += value;
+});
+clrAndDel.forEach((button) => {
+  let value = button.value;
+  button.textContent = value.toUpperCase()
+  button.onclick = () => mainDisplay.textContent += value;
+});

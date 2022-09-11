0c2041044aecf266b7239bc428bf7c3423c3307d
oleolerato <olerato.mosikidi@gmail.com>
2022-09-11 12:45:40 +0200

    Target html elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const subDisplay = document.querySelector('.subDisplay')
+const mainDisplay = document.querySelector('.mainDisplay')
+const numerals = document.querySelectorAll('.numerals button')
+const operators = document.querySelectorAll('.operators button')
+const clrAndDel = document.querySelectorAll('.clearAndDel button')
+

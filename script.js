2b09f63fe6d2c48f2af6d3e7a1ac1859b41c9f91
oleolerato <olerato.mosikidi@gmail.com>
2022-09-11 12:56:27 +0200

    Declare the variables; currentOperation, firstOperand and secondOperand

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const subDisplay = document.querySelector('.subDisplay')
-const mainDisplay = document.querySelector('.mainDisplay')
-const numerals = document.querySelectorAll('.numerals button')
-const operators = document.querySelectorAll('.operators button')
-const clrAndDel = document.querySelectorAll('.clearAndDel button')
+const subDisplay = document.querySelector('.subDisplay');
+const mainDisplay = document.querySelector('.mainDisplay');
+const numerals = document.querySelectorAll('.numerals button');
+const operators = document.querySelectorAll('.operators button');
+const clrAndDel = document.querySelectorAll('.clearAndDel button');
 
+let currentOperation = none;
+let firstOperand = 0
+let secondOperand = 0
